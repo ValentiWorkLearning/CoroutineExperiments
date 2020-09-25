c502d570c2eb234e1bd8d81ef1eae785066ee6f2
VKorniienko <VKorniienko@VKORNIIENKO>
2020-09-25 09:25:45 +0300

    Added draft of coroutine SPI

--- main.cpp
+++ main.cpp
@@ -7,6 +7,16 @@
 #include <cstdint>
 
 // https://blog.panicsoftware.com/your-first-coroutine/
+// https://manybutfinite.com/post/anatomy-of-a-program-in-memory/
+// https://manybutfinite.com/post/journey-to-the-stack/
+// https://luncliff.github.io/posts/Exploring-MSVC-Coroutine.html
+// https://habr.com/en/post/493808/ // coroutine code linearizing
+// https://lewissbaker.github.io/2017/09/25/coroutine-theory
+// https://lewissbaker.github.io/2018/09/05/understanding-the-promise-type
+// https://devblogs.microsoft.com/oldnewthing/20191219-00/?p=103230
+// https://habr.com/en/post/348602/ // boost asio with coroutines TS
+// https://m.habr.com/ru/post/519464/ // C++20 coroutines
+// https://mariusbancila.ro/blog/2020/06/22/a-cpp20-coroutine-example/
 
 // We have to define the 'resumable' class for the way
 // of communicating between the coroutine and usual function
@@ -75,25 +85,25 @@ resumable foo()
     std::cout<<"from coroutine";
 }
 
-//
-//void spiTrasnmitBuffer(
-//        std::uint8_t* _pBuffer
-//    ,   std::uint16_t _bufferSize
-//)
-//{
-//    using namespace std::chrono_literals;
-//    std::this_thread::sleep_for(100ms);
-//}
-//
-//auto commandBufferFirst = std::array{ 0x00u, 0x01u, 0x02u, 0x03u };
-//auto commandBufferSecond = std::array{ 0x04u, 0x05u, 0x06u, 0x07u,0x08u };
-//
-//void initDisplay()
-//{
-//    co_await spiTrasnmitBuffer(commandBufferFirst.data(), commandBufferFirst.size());
-//    co_await spiTrasnmitBuffer(commandBufferSecond.data(), commandBufferSecond.size());
-//}
-//
+
+void spiTrasnmitBuffer(
+        std::uint8_t* _pBuffer
+    ,   std::uint16_t _bufferSize
+)
+{
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(100ms);
+}
+
+auto commandBufferFirst = std::array{ 0x00u, 0x01u, 0x02u, 0x03u };
+auto commandBufferSecond = std::array{ 0x04u, 0x05u, 0x06u, 0x07u,0x08u };
+
+void initDisplay()
+{
+    co_await spiTrasnmitBuffer(commandBufferFirst.data(), commandBufferFirst.size());
+    co_await spiTrasnmitBuffer(commandBufferSecond.data(), commandBufferSecond.size());
+}
+
 
 //For using co_await operator we have to use the concept of 'awaitable entity'
 //the main idea is quite simple - to provide the implementation for the customization_point of the coroutine
